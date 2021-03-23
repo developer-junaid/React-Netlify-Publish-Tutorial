e9c8a9a3a73f32daf9d1604e7a37898bdae41417
developer-junaid <qjunaid623@gmail.com>
2021-03-23 11:58:39 +0500

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello World</h2>
+      <p>
+        This is tutorial that teaches you how to deploy react site to netlify,
+        using netlify.toml configuration file and netlify GUI, with CI/CD
+      </p>
     </div>
   );
 }
